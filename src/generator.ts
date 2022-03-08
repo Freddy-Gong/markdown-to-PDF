ec53c48cd81d322c2446517231a64b4c43eb0839
Freddy-Gong <21914103@zju.edu.cn>
2022-03-08 15:53:22 +0800

    基本方法确认

--- src/generator.ts
+++ src/generator.ts
@@ -1,16 +1,22 @@
+import { Canvas } from 'canvas'
 import path from 'path'
 import fs from 'fs'
-import { Canvas } from 'canvas'
-// 饼图需要自定义的参数
 
+function generatorCnavas(markdownContent: string) {
+
+}
+
+const absolutePath = path.resolve(__dirname, '../test/test.md')
+const fileContent = fs.readFileSync(absolutePath, "utf-8")
 // 饼图 方法定义
 function pie() {
     var canvas = new Canvas(1240, 1754);
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = "#fff"
     ctx.fillRect(0, 0, 1240, 1754)
-    ctx.font = '48px serif';
-    ctx.strokeText("Hello world", 10, 50)
+    ctx.fillStyle = "black"
+    ctx.font = '19px serif';
+    ctx.fillText(fileContent, 10, 50)
 
     console.log(canvas.toDataURL());//生成图片base64
     canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'pie.png'))) // 生成本地图片(指定文件名)
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import path from 'path'
 import fs from 'fs'
-import { marked } from 'marked'
 
 const absolutePath = path.resolve(__dirname, '../test/test.md')
-console.log(absolutePath)
 const fileContent = fs.readFileSync(absolutePath, "utf-8")
-console.log(fileContent)
-const htmlContent = marked.parse(fileContent)
-console.log(htmlContent)
-HTMLElement
-// const canvas = document.createElement("canvas")
-// console.log(canvas)
+for (let i = 0; i < fileContent.length; i++) {
+    console.log(fileContent[i])
+    if (fileContent[i] === "\n") {
+        console.log("回车")
+    }
+    if (fileContent[i] === " ") {
+        console.log("空格")
+    }
+}
+
